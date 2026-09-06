eac058cccf83c800a42e33a001630b3e0acfd40b
Apoorv Gupta <guptaapoorv2005@gmail.com>
2026-09-06 16:56:17 +0530

    refactor: improve formatting and readability in ApiError class

--- backend/src/utils/ApiError.js
+++ backend/src/utils/ApiError.js
@@ -1,18 +1,18 @@
-class ApiError extends Error{
-    constructor(statusCode, message = "Something went wrong", errors = [], stack = ""){
-        super(message)     //jab class extend karte hain toh super constructor call karna padhta hai ek baar varna this use nhi kar sakte
+class ApiError extends Error {
+    constructor(statusCode, message = "Something went wrong", errors = [], stack = "") {
+        super(message)
         this.statusCode = statusCode
         this.data = null
         this.success = false
         this.errors = errors
 
-        if(stack){
+        if (stack) {
             this.stack = stack
         }
-        else{
+        else {
             Error.captureStackTrace(this, this.constructor)
         }
     }
 }
 
-export {ApiError}
+export { ApiError }
